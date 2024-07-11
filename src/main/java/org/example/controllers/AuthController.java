860ec5c47f6425aa4c194d99cab2f2e1e7c4d58a
thomkainos <thomas.phan@kainos.com>
2024-07-11 14:35:11 +0100

    maven dependencies

--- src/main/java/org/example/controllers/AuthController.java
+++ src/main/java/org/example/controllers/AuthController.java
@@ -1,4 +1,52 @@
 package org.example.controllers;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiKeyAuthDefinition;
+import io.swagger.annotations.SecurityDefinition;
+import io.swagger.annotations.SwaggerDefinition;
+import org.example.models.LoginRequest;
+import org.example.services.AuthService;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Api("Auth API")
+@Path("/api/auth")
+@SwaggerDefinition(
+        securityDefinition = @SecurityDefinition(
+                apiKeyAuthDefinitions = {
+                        @ApiKeyAuthDefinition(
+                                key = HttpHeaders.AUTHORIZATION,
+                                name = HttpHeaders.AUTHORIZATION,
+                                in = ApiKeyAuthDefinition.ApiKeyLocation.HEADER
+                        )
+                }
+        )
+)
 public class AuthController {
+    AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @POST
+    @Path("/login")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response login(LoginRequest loginRequest) {
+        try {
+            return Response.ok().entity(auth)
+        }
+    }
+
+//    @POST
+//    @Path("/register")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    @RolesAllowed({})
 }
+
