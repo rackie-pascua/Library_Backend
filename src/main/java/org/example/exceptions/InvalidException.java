fbf78b122aba0912440748bf2364b77bd26e7d61
mark-mcilroykainos <mark.mcilroy@kainos.com>
2024-07-11 14:49:10 +0100

    fixed exception merge conflict

--- src/main/java/org/example/exceptions/InvalidException.java
+++ src/main/java/org/example/exceptions/InvalidException.java
@@ -1,5 +1,12 @@
 package org.example.exceptions;
 
 
-public class InvalidException {
+
+
+
+public class InvalidException extends Throwable{
+    public InvalidException (Entity entity, String reason){
+        super(entity.getEntity() + " is not valid");
+    }
+
 }
--- src/main/java/org/example/services/AuthService.java
+++ src/main/java/org/example/services/AuthService.java
@@ -1,4 +1,45 @@
 package org.example.services;
 
+
+import io.jsonwebtoken.Jwts;
+import org.example.daos.AuthDao;
+import org.example.exceptions.InvalidException;
+import org.example.models.LoginRequest;
+import org.example.models.User;
+
+import java.sql.Date;
+import java.sql.SQLException;
+
 public class AuthService {
+
+    private AuthDao authDao;
+
+    public AuthService(AuthDao authDao){
+        this.authDao = authDao;
+    }
+
+    public String login (LoginRequest loginRequest) throws SQLException, InvalidException {
+
+        User user = authDao.getUser(loginRequest);
+
+        if (user == null){
+            throw new InvalidException(Entity.USER, "Invalid credentials");
+        }
+        return generateJwtToken(user);
+
+    }
+
+    private String generateJwtToken(User user){
+        return Jwts.builder()
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 28800000))
+                .claim("Role", user.getSystemRoleId())
+                .subject(user.getUsername())
+                .issuer("DropwizardDemo")
+                .signWith(Jwts.SIG.HS256.key().build())
+                .compact();
+
+    }
+
+
 }
